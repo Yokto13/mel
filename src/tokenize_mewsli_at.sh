4113bf9216a22a86a19895ad1357ced0eda3cd0c
Dominik Farhan <nikousf@seznam.cz>
2024-07-07 14:23:07 +0200

    fix run action name

--- src/tokenize_mewsli_at.sh
+++ src/tokenize_mewsli_at.sh
@@ -4,4 +4,4 @@ set -ueo pipefail
 
 VENV=../venv/bin/activate
 source $VENV
-python run_action.py tokens_all_mewsli_at "/home/farhand/bc/data/mewsli/mewsli-9/output/dataset" tokens_mewsli_at 1 64 "setu4993/LEALLA-base"
+python run_action.py tokens_for_all_mewsli_at "/home/farhand/bc/data/mewsli/mewsli-9/output/dataset" tokens_mewsli_at 1 64 "setu4993/LEALLA-base"
