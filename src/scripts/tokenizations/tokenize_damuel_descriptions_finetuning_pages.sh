fb43cafabea690b3eabad911f1235ab369d96820
Dominik Farhan <nikousf@seznam.cz>
2024-08-20 16:25:57 +0200

    fix(tokens): :bug: correct ending of damuel path

--- src/scripts/tokenizations/tokenize_damuel_descriptions_finetuning_pages.sh
+++ src/scripts/tokenizations/tokenize_damuel_descriptions_finetuning_pages.sh
@@ -9,4 +9,4 @@ source $VENV
 
 RESULT="$OUTPUTS/tokens_damuel_finetuning"
 
-python run_action.py tokens_for_all_damuel_finetuning_pages "$DAMUEL" "$RESULT" 100 64 "/lnet/work/home-students-external/farhan/troja/outputs/models/LEALLA-small"
+python run_action.py tokens_for_all_damuel_finetuning_pages "$DAMUEL/1.0" "$RESULT" 100 64 "/lnet/work/home-students-external/farhan/troja/outputs/models/LEALLA-small"
