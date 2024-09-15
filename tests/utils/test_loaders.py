8fc7542ff3dd925c75e64bdecabb3298b5b54b13
Dominik Farhan <nikousf@seznam.cz>
2024-09-15 21:07:20 +0200

    feat: :white_check_mark: add loaders sort stability check

--- tests/utils/test_loaders.py
+++ tests/utils/test_loaders.py
@@ -130,3 +130,43 @@ def test_loaders_sort_corresponding(loader_func, file_name, test_data, use_strin
 
         for qid, emb in loaded_data:
             assert np.array_equal(emb, qid_emb_test_data[qid])
+
+
+@pytest.mark.parametrize(
+    "loader_func, file_name, test_data",
+    [
+        (
+            load_embs_and_qids,
+            "embs_qids.npz",
+            {
+                "embs": np.array([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]]),
+                "qids": np.array([300, 100, 200]),
+            },
+        ),
+        (
+            load_embs_qids_tokens,
+            "embs_qids_tokens.npz",
+            {
+                "embs": np.array([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]]),
+                "qids": np.array([300, 100, 200]),
+                "tokens": np.array([[1, 2], [3, 4], [5, 6]]),
+            },
+        ),
+    ],
+)
+@pytest.mark.parametrize("use_string_path", [True, False])
+def test_loaders_sort_stable(loader_func, file_name, test_data, use_string_path):
+    with tempfile.TemporaryDirectory() as temp_dir:
+        dir_path = Path(temp_dir)
+        if use_string_path:
+            dir_path = str(dir_path)
+        file_path = Path(dir_path) / file_name
+
+        np.savez_compressed(file_path, **test_data)
+
+        loaded_data = loader_func(dir_path)
+        loaded_data2 = loader_func(dir_path)
+        loaded_data3 = loader_func(dir_path)
+
+        assert np.array_equal(loaded_data, loaded_data2)
+        assert np.array_equal(loaded_data, loaded_data3)
