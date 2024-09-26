d20bc65674551e08be39610d57b4647fb3bc5223
Dominik Farhan <nikousf@seznam.cz>
2024-09-26 12:22:48 +0200

    feat(tokens): :sparkles: gin for all tokenization

---
+++ src/run_action_gin.py
@@ -0,0 +1,188 @@
+from functools import partial
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+# os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+
+from fire import Fire
+import gin
+
+import wandb
+
+from baselines.alias_table.all_languages import all_languages
+from baselines.alias_table.one_language_lemma import alias_table_with_lemmas
+from baselines.alias_table.from_tokens import one_language
+from baselines.alias_table.string_similarity import string_similarity
+from baselines.olpeat.at_embeddings import embs_from_tokens_and_model_name_at
+from baselines.olpeat.meludr_olpeat import meludr_olpeat
+from baselines.olpeat.meludr_evaluate import meludr_run_recall_calculation
+from baselines.olpeat.find_recall import find_recall as find_recall_olpeat
+
+from utils.embeddings import (
+    embs_from_tokens_and_model_name,
+    embs_from_tokens_model_name_and_state_dict,
+)
+
+from data_processors.tokens.duplicates_filter_script import run_duplicates_filter_script
+
+from finetunings.generate_epochs.generate import generate
+from finetunings.generate_epochs.embed_links_for_generation import (
+    embed_links_for_generation,
+)
+from finetunings.finetune_model.train import train
+from finetunings.finetune_model.train_ddp import train_ddp
+from finetunings.evaluation.evaluate import evaluate, run_recall_calculation
+from finetunings.file_processing.gathers import move_tokens, rename, remove_duplicates
+
+from multilingual_dataset.creator import create_multilingual_dataset, run_kb_creator
+from multilingual_dataset.combine_embs import combine_embs_by_qid
+
+from tokenization.generate_tokens import (
+    tokens_for_finetuning_mewsli,
+    tokens_for_finetuning_damuel_descriptions,
+    tokens_for_finetuning_damuel_links,
+    tokens_for_at_descriptions,
+    tokens_for_at_links,
+    tokens_for_at_mewsli,
+)
+
+from tokenization.tokenize_mewsli import tokens_for_all_mewsli
+from tokenization.tokenize_damuel import tokens_for_all_damuel
+from tokenization.runner import (
+    run_damuel_description_context,
+    run_damuel_description_mention,
+    run_damuel_link_context,
+    run_damuel_link_mention,
+    run_mewsli_mention,
+    run_mewsli_context,
+    run_damuel_mention,
+)
+
+
+tokens_for_all_mewsli_at = partial(tokens_for_all_mewsli, ignore_context=True)
+tokens_for_all_damuel_at = partial(tokens_for_all_damuel, ignore_context=True)
+tokens_for_all_damuel_at_pages = partial(
+    tokens_for_all_damuel, ignore_context=True, only_pages=True
+)
+tokens_for_all_mewsli_finetuning = partial(tokens_for_all_mewsli, ignore_context=False)
+tokens_for_all_damuel_finetuning = partial(tokens_for_all_damuel, ignore_context=False)
+tokens_for_all_damuel_finetuning_pages = partial(
+    tokens_for_all_damuel, ignore_context=False, only_pages=True
+)
+
+from utils.arg_names import get_args_names
+from utils.validate_tokens import validate_tokens
+
+print("Imports finished")
+
+
+def choose_action(action):
+    match action:
+        case "generate":
+            return generate
+        case "train":
+            return train
+        case "train_ddp":
+            return train_ddp
+        case "evaluate":
+            return evaluate
+        case "copy":
+            return move_tokens
+        case "at_lemmas":
+            return alias_table_with_lemmas
+        case "at_one":
+            return one_language
+        case "at_all":
+            return all_languages
+        case "string_similarity":
+            return string_similarity
+        case "recalls":
+            return run_recall_calculation
+        case "meludr_recalls":
+            return meludr_run_recall_calculation
+        case "rename":
+            return rename
+        case "remove_duplicates":
+            return remove_duplicates
+        case "filter_duplicates_script":
+            return run_duplicates_filter_script
+        case "tokens_mewsli":
+            return tokens_for_finetuning_mewsli
+        case "tokens_descriptions":
+            return tokens_for_finetuning_damuel_descriptions
+        case "tokens_links":
+            return tokens_for_finetuning_damuel_links
+        case "tokens_descriptions_at":
+            return tokens_for_at_descriptions
+        case "tokens_links_at":
+            return tokens_for_at_links
+        case "tokens_mewsli_at":
+            return tokens_for_at_mewsli
+        case "tokens_for_all_mewsli_at":
+            return tokens_for_all_mewsli_at
+        case "tokens_for_all_damuel_at":
+            return tokens_for_all_damuel_at
+        case "tokens_for_all_damuel_at_pages":
+            return tokens_for_all_damuel_at_pages
+        case "tokens_for_all_mewsli_finetuning":
+            return tokens_for_all_mewsli_finetuning
+        case "tokens_for_all_damuel_finetuning":
+            return tokens_for_all_damuel_finetuning
+        case "tokens_for_all_damuel_finetuning_pages":
+            return tokens_for_all_damuel_finetuning_pages
+        case "embs_from_tokens_and_model_name":
+            return embs_from_tokens_and_model_name
+        case "meludr_olpeat":
+            return meludr_olpeat
+        case "embs_from_tokens_and_model_name_at":
+            return embs_from_tokens_and_model_name_at
+        case "find_recall_olpeat":
+            return find_recall_olpeat
+        case "embs_from_tokens_model_name_and_state_dict":
+            return embs_from_tokens_model_name_and_state_dict
+        case "embed_links_for_generation":
+            return embed_links_for_generation
+        case "create_multilingual_dataset":
+            return create_multilingual_dataset
+        case "run_kb_creator":
+            return run_kb_creator
+        case "combine_embs_by_qid":
+            return combine_embs_by_qid
+        case "run_mewsli_mention":
+            return run_mewsli_mention
+        case "run_mewsli_context":
+            return run_mewsli_context
+        case "validate_tokens":
+            return validate_tokens
+        case "run_damuel_description_mention":
+            return run_damuel_description_mention
+        case "run_damuel_description_context":
+            return run_damuel_description_context
+        case "run_damuel_link_context":
+            return run_damuel_link_context
+        case "run_damuel_link_mention":
+            return run_damuel_link_mention
+        case "run_damuel_mention":
+            return run_damuel_mention
+        case _:
+            raise ValueError(f"Unknown action: {action}")
+
+
+def main(*args):
+    action_descriptor = args[0]
+    action = choose_action(action_descriptor)
+    arg_names = get_args_names(action)
+    print(arg_names)
+    wandb.init(
+        project=f"EL-{args[0]}",
+        config={"action": args[0]}
+        | {arg_name: arg_value for arg_name, arg_value in zip(arg_names, args[1:])},
+    )
+    print(f"Running {action_descriptor} with args {args[1:]}")
+    gin.parse_config_file(args[1])
+    action()
+
+
+if __name__ == "__main__":
+    Fire(main)
--- src/tokenization/runner.py
+++ src/tokenization/runner.py
@@ -1,3 +1,5 @@
+import os
+import glob
 import multiprocessing
 from typing import List
 import logging
@@ -13,6 +15,7 @@ from tokenization.pipeline.pipelines import (
     Pipeline,
     DamuelDescriptionMentionPipeline,
     MewsliMentionPipeline,
+    DamuelLinkMentionPipeline,
 )
 
 logging.basicConfig(level=logging.INFO)
@@ -29,77 +32,80 @@ def run_pipelines(
         pool.map(run_pipeline, pipelines)
 
 
-def run_mewsli_mention() -> None:
-    tokenizer = AutoTokenizer.from_pretrained(
-        "/lnet/work/home-students-external/farhan/troja/outputs/models/LEALLA-base"
-    )
-    expected_size = 64
-
-    output_base_dir = "/lnet/work/home-students-external/farhan/troja/outputs/pipelines/mewsli_mention"
-    languages = ["ar", "de", "en", "es", "fa", "ja", "sr", "ta", "tr"]
-
-    for lang in languages:
-        os.makedirs(os.path.join(output_base_dir, lang), exist_ok=True)
-
-    pipelines_dict = {
-        lang: [
-            MewsliMentionPipeline(
-                mewsli_tsv_path=f"/lnet/work/home-students-external/farhan/mewsli/mewsli-9/output/dataset/{lang}/mentions.tsv",
-                tokenizer=tokenizer,
-                expected_size=expected_size,
-                output_filename=f"{output_base_dir}/{lang}/tokens_qids.npz",
-                compress=True,
-            )
-        ]
-        for lang in languages
-    }
-
-    num_processes = 9
-    for lang, pipelines in pipelines_dict.items():
-        run_pipelines(pipelines, num_processes)
-        logging.info(f"Finished processing language: {lang}")
-
-
-def run_mewsli_context() -> None:
-    tokenizer = AutoTokenizer.from_pretrained(
-        "/lnet/work/home-students-external/farhan/troja/outputs/models/LEALLA-base"
-    )
-    expected_size = 64
-
-    output_base_dir = "/lnet/work/home-students-external/farhan/troja/outputs/pipelines/mewsli_finetuning"
-    languages = ["ar", "de", "en", "es", "fa", "ja", "sr", "ta", "tr"]
+@gin.configurable
+def run_mewsli_mention(
+    model_path: str,
+    expected_size: int,
+    output_base_dir: str,
+    languages: List[str],
+    mewsli_dataset_path: str,
+    compress: bool,
+    num_processes: int,
+) -> None:
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
 
     for lang in languages:
         os.makedirs(os.path.join(output_base_dir, lang), exist_ok=True)
 
-    pipelines_dict = {
-        lang: [
-            MewsliContextPipeline(
-                mewsli_tsv_path=f"/lnet/work/home-students-external/farhan/mewsli/mewsli-9/output/dataset/{lang}/mentions.tsv",
-                tokenizer=tokenizer,
-                label_token="[M]",
-                expected_size=expected_size,
-                output_filename=f"{output_base_dir}/{lang}/tokens_qids.npz",
-                compress=True,
-            )
-        ]
+    pipelines = [
+        MewsliMentionPipeline(
+            mewsli_tsv_path=os.path.join(mewsli_dataset_path, lang, "mentions.tsv"),
+            tokenizer=tokenizer,
+            expected_size=expected_size,
+            output_filename=f"{output_base_dir}/{lang}/tokens_qids.npz",
+            compress=compress,
+        )
         for lang in languages
-    }
+    ]
 
-    num_processes = 9
-    for lang, pipelines in pipelines_dict.items():
-        run_pipelines(pipelines, num_processes)
-        logging.info(f"Finished processing language: {lang}")
+    run_pipelines(pipelines, num_processes)
+    logging.info("Finished processing all languages for Mewsli Mention")
 
 
-def run_damuel_description_mention() -> None:
-    tokenizer = AutoTokenizer.from_pretrained(
-        "/lnet/work/home-students-external/farhan/troja/outputs/models/LEALLA-base"
-    )
-    expected_size = 64
+@gin.configurable
+def run_mewsli_context(
+    model_path: str,
+    expected_size: int,
+    output_base_dir: str,
+    languages: List[str],
+    mewsli_dataset_path: str,
+    label_token: str,
+    compress: bool,
+    num_processes: int,
+) -> None:
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
 
-    output_base_dir = "/lnet/work/home-students-external/farhan/troja/outputs/pipelines/damuel_description_mention"
-    languages = ["es"]
+    for lang in languages:
+        os.makedirs(os.path.join(output_base_dir, lang), exist_ok=True)
+
+    pipelines = [
+        MewsliContextPipeline(
+            mewsli_tsv_path=os.path.join(mewsli_dataset_path, lang, "mentions.tsv"),
+            tokenizer=tokenizer,
+            label_token=label_token,
+            expected_size=expected_size,
+            output_filename=f"{output_base_dir}/{lang}/tokens_qids.npz",
+            compress=compress,
+        )
+        for lang in languages
+    ]
+
+    run_pipelines(pipelines, num_processes)
+    logging.info("Finished processing all languages for Mewsli Context")
+
+
+@gin.configurable
+def run_damuel_description_mention(
+    model_path: str,
+    expected_size: int,
+    output_base_dir: str,
+    languages: List[str],
+    damuel_base_path: str,
+    compress: bool,
+    num_processes: int,
+    remainder_mod: int,
+) -> None:
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
 
     for lang in languages:
         os.makedirs(os.path.join(output_base_dir, lang), exist_ok=True)
@@ -107,20 +113,19 @@ def run_damuel_description_mention() -> None:
     pipelines_dict = {
         lang: [
             DamuelDescriptionMentionPipeline(
-                damuel_path=f"/lnet/work/home-students-external/farhan/damuel/1.0-xz/damuel_1.0_{lang}",
+                damuel_path=f"{damuel_base_path}/damuel_1.0_{lang}",
                 tokenizer=tokenizer,
                 expected_size=expected_size,
                 output_filename=f"{output_base_dir}/{lang}/tokens_qids_{i}.npz",
-                compress=True,
+                compress=compress,
                 remainder=i,
-                mod=10,
+                mod=remainder_mod,
             )
-            for i in range(10)
+            for i in range(remainder_mod)
         ]
         for lang in languages
     }
 
-    num_processes = 9
     for lang, pipelines in pipelines_dict.items():
         run_pipelines(pipelines, num_processes)
         logging.info(f"Finished processing language: {lang}")
@@ -131,7 +136,7 @@ def run_damuel_description_context(
     model_path: str,
     expected_size: int,
     output_base_dir: str,
-    languages: list[str],
+    languages: List[str],
     damuel_base_path: str,
     label_token: str,
     compress: bool,
@@ -149,7 +154,7 @@ def run_damuel_description_context(
                 damuel_path=f"{damuel_base_path}/damuel_1.0_{lang}",
                 tokenizer=tokenizer,
                 expected_size=expected_size,
-                output_filename=f"{output_base_dir}/{lang}/tokens_qids_{i}.npz",
+                output_filename=f"{output_base_dir}/{lang}/descs_pages/tokens_qids_{i}.npz",
                 label_token=label_token,
                 compress=compress,
                 remainder=i,
@@ -165,39 +170,135 @@ def run_damuel_description_context(
         logging.info(f"Finished processing language: {lang}")
 
 
-def run_damuel_link_context() -> None:
-    tokenizer = AutoTokenizer.from_pretrained(
-        "/lnet/work/home-students-external/farhan/troja/outputs/models/LEALLA-base"
-    )
-    expected_size = 64
+@gin.configurable
+def run_damuel_link_context(
+    model_path: str,
+    expected_size: int,
+    output_base_dir: str,
+    languages: List[str],
+    damuel_base_path: str,
+    label_token: str,
+    compress: bool,
+    remainder_mod: int,
+    num_processes: int,
+) -> None:
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
 
-    output_base_dir = "/lnet/work/home-students-external/farhan/troja/outputs/pipelines/damuel_link_context"
-    languages = ["es"]
+    for lang in languages:
+        os.makedirs(os.path.join(output_base_dir, lang, "links"), exist_ok=True)
+
+    pipelines_dict = {
+        lang: [
+            DamuelLinkContextPipeline(
+                damuel_path=f"{damuel_base_path}/damuel_1.0_{lang}",
+                tokenizer=tokenizer,
+                expected_size=expected_size,
+                output_filename=f"{output_base_dir}/{lang}/links/tokens_qids_{i}.npz",
+                label_token=label_token,
+                compress=compress,
+                remainder=i,
+                mod=remainder_mod,
+            )
+            for i in range(remainder_mod)
+        ]
+        for lang in languages
+    }
+
+    for lang, pipelines in pipelines_dict.items():
+        run_pipelines(pipelines, num_processes)
+        logging.info(f"Finished processing language: {lang}")
+
+
+@gin.configurable
+def run_damuel_link_mention(
+    model_path: str,
+    expected_size: int,
+    output_base_dir: str,
+    languages: List[str],
+    damuel_base_path: str,
+    compress: bool,
+    num_processes: int,
+    require_link_wiki_origin: bool,
+    remainder_mod: int,
+) -> None:
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
 
     for lang in languages:
         os.makedirs(os.path.join(output_base_dir, lang), exist_ok=True)
 
     pipelines_dict = {
         lang: [
-            DamuelLinkContextPipeline(
-                damuel_path=f"/lnet/work/home-students-external/farhan/damuel/1.0-xz/damuel_1.0_{lang}",
+            DamuelLinkMentionPipeline(
+                damuel_path=f"{damuel_base_path}/damuel_1.0_{lang}",
                 tokenizer=tokenizer,
                 expected_size=expected_size,
                 output_filename=f"{output_base_dir}/{lang}/tokens_qids_{i}.npz",
-                label_token="[M]",
-                compress=True,
+                compress=compress,
                 remainder=i,
-                mod=90,
+                mod=remainder_mod,
+                require_link_wiki_origin=require_link_wiki_origin,
             )
-            for i in range(90)
+            for i in range(remainder_mod)
         ]
         for lang in languages
     }
 
-    num_processes = 90
     for lang, pipelines in pipelines_dict.items():
         run_pipelines(pipelines, num_processes)
         logging.info(f"Finished processing language: {lang}")
+
+
+@gin.configurable
+def run_damuel_mention(
+    model_path: str,
+    expected_size: int,
+    output_base_dir: str,
+    languages: List[str],
+    damuel_base_path: str,
+    compress: bool,
+    num_processes: int,
+    remainder_mod: int,
+    require_link_wiki_origin: bool,
+) -> None:
+
+    # Run description mention tokenization
+    run_damuel_description_mention(
+        model_path=model_path,
+        expected_size=expected_size,
+        output_base_dir=output_base_dir,
+        languages=languages,
+        damuel_base_path=damuel_base_path,
+        compress=compress,
+        num_processes=num_processes,
+        remainder_mod=remainder_mod,
+    )
+
+    # Rename description mention files
+    for lang in languages:
+        lang_dir = os.path.join(output_base_dir, lang)
+        for file in glob.glob(os.path.join(lang_dir, "tokens_qids_*.npz")):
+            new_name = file.replace("tokens_qids_", "tokens_qids_descs_")
+            os.rename(file, new_name)
+
+    # Run link mention tokenization
+    run_damuel_link_mention(
+        model_path=model_path,
+        expected_size=expected_size,
+        output_base_dir=output_base_dir,
+        languages=languages,
+        damuel_base_path=damuel_base_path,
+        compress=compress,
+        num_processes=num_processes,
+        require_link_wiki_origin=require_link_wiki_origin,
+        remainder_mod=remainder_mod,
+    )
+
+    # Rename link mention files
+    for lang in languages:
+        lang_dir = os.path.join(output_base_dir, lang)
+        for file in glob.glob(os.path.join(lang_dir, "tokens_qids_*.npz")):
+            new_name = file.replace("tokens_qids_", "tokens_qids_links_")
+            os.rename(file, new_name)
 
 
 if __name__ == "__main__":
