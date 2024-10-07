69a0eb6682221c19828a226d5da98befdc14d3d1
Dominik Farhan <nikousf@seznam.cz>
2024-10-07 07:38:56 +0200

    fix(train): :poop: increase links per epoch

--- src/scripts/train/all_langs.sh
+++ src/scripts/train/all_langs.sh
@@ -46,7 +46,7 @@ run_ml_finetuning_round() {
     local POS=1
 
     # Multiple by 2 to make sure that if a link contained something faulty we can skip it.
-    local LINKS_PER_ROUND=$(($STEPS_PER_EPOCH * 10))
+    local LINKS_PER_ROUND=$(($STEPS_PER_EPOCH * 200))
     echo "LPR $LINKS_PER_ROUND"
 
     local ACTION_SCRIPT="run_action_gin.py $MODEL_CONFIG_PATH $TRAIN_CONFIG_PATH"
