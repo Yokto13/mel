9b4bf309e6fdaebd6af093c1300cee2129e26295
Dominik Farhan <nikousf@seznam.cz>
2024-10-17 11:04:20 +0200

    refactor: :coffin: remove dead scripts

--- src/scripts/el100_entities/README.md
+++
@@ -1,1 +0,0 @@
-Scripts for building KB similarly to [Mewsli-9 paper](https://aclanthology.org/2020.emnlp-main.630/).
--- src/scripts/meludr/olpeat/olpeat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ueo pipefail
-
-languages=(ar de en es fa ja sr ta tr)
-
-for lang in "${languages[@]}"; do
-    ./olpeat_lang.sh "$lang" &
-done
