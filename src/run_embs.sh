0e58f943cd2c554e2c433c94c9b4a0c94e1d1e32
Dominik Farhan <nikousf@seznam.cz>
2024-10-17 10:53:47 +0200

    refactor: :coffin: Remove scripts that are no longer used
    We might have to revert this commit if we discover that some other dependency needs these

--- src/run_embs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ueo pipefail
-
-source ../venv/bin/activate
-
-python run_action.py "embs_from_tokens_and_model_name" "$DAMUEL/1.0/damuel_1.0_ja/links" "setu4993/LEALLA-base" 4096 "$OUTPUTS/olpeat_data/ja_dam"
