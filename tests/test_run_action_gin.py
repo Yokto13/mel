cfa9fc09552170642656d872962706db810e1121
Dominik Farhan <nikousf@seznam.cz>
2024-10-17 14:11:00 +0200

    feat: :white_check_mark: new test of imports

--- tests/test_run_action_gin.py
+++ tests/test_run_action_gin.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.mark.slow
 def test_imports_work():
     try:
-        from run_action import main
+        from run_action_gin import main
 
         assert True
     except ImportError as e:
