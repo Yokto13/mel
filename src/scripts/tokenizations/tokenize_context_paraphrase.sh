37f6ca786177b2f6ddaf1c874ad9ce442631ba16
Dominik Farhan <nikousf@seznam.cz>
2024-11-09 12:13:05 +0100

    feat: :art: script update

---
+++ src/scripts/tokenizations/tokenize_context_paraphrase.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Tokenizes mentions for both DaMuEL and Mewsli.
+# This tokenization works only on mentions which makes it suitable with OLPEAT.
+
+set -ueo pipefail
+
+cd ../../
+
+source ../venv/bin/activate
+
+python run_action_gin.py ../configs/lealla.gin ../configs/tokenization_context.gin run_damuel_description_context
+python run_action_gin.py ../configs/lealla.gin ../configs/tokenization_context.gin run_damuel_link_context
+
+python run_action_gin.py ../configs/lealla.gin ../configs/tokenization_context.gin run_mewsli_context
