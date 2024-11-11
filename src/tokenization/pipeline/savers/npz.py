32f4465bc8824986b950c991f3ab073815803012
Dominik Farhan <nikousf@seznam.cz>
2024-11-11 14:46:16 +0100

    feat(tokens): :sparkles: add NpzIncrementalSaver

--- src/tokenization/pipeline/savers/npz.py
+++ src/tokenization/pipeline/savers/npz.py
@@ -19,12 +19,52 @@ class NPZSaver(PipelineStep):
             tokens_list.append(tokens)
             qids_list.append(qids)
 
+        self._save_data(tokens_list, qids_list)
+        yield  # To comply with the generator interface
+
+    def _save_data(self, tokens_list: list, qids_list: list) -> None:
+        filename = self.get_current_filename()
         tokens_array = np.array(tokens_list)
         qids_array = np.array(qids_list)
-
         if self.compress:
-            np.savez_compressed(self.filename, tokens=tokens_array, qids=qids_array)
+            np.savez_compressed(filename, tokens=tokens_array, qids=qids_array)
         else:
-            np.savez(self.filename, tokens=tokens_array, qids=qids_array)
+            np.savez(filename, tokens=tokens_array, qids=qids_array)
+
+    def get_current_filename(self) -> str:
+        return self.filename
+
+
+class NPZSaverIncremental(NPZSaver):
+    def __init__(self, filename: str, compress: bool = False, save_every: int = 10000):
+        self.filename = filename
+        self.compress = compress
+        self.save_every = save_every
+        self.counter = 0
+
+    def process(
+        self, input_gen: Generator[tuple, None, None]
+    ) -> Generator[None, None, None]:
+        tokens_list = []
+        qids_list = []
+        for tokens, qids in input_gen:
+            tokens_list.append(tokens)
+            qids_list.append(qids)
+
+            if len(tokens_list) >= self.save_every:
+                self._save_data(tokens_list, qids_list)
+                tokens_list = []
+                qids_list = []
+
+        if len(tokens_list) > 0:
+            self._save_data(tokens_list, qids_list)
 
         yield  # To comply with the generator interface
+
+    def get_current_filename(self) -> str:
+        if "npz" in self.filename:
+            filename = self.filename[:-4]
+        else:
+            filename = self.filename
+        self.counter += 1
+        return f"{filename}_{self.counter}.npz"
--- tests/tokenization/pipeline/savers/test_npz.py
+++ tests/tokenization/pipeline/savers/test_npz.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import pytest
 
-from tokenization.pipeline.savers.npz import NPZSaver
+from tokenization.pipeline.savers.npz import NPZSaver, NPZSaverIncremental
 
 
 class TestNPZSaver:
@@ -52,3 +52,28 @@ class TestNPZSaver:
         list(saver.process(input_gen))
 
         assert os.path.exists(expected_filename)
+
+
+class TestNPZSaverIncremental:
+    @pytest.fixture
+    def saver_data(self):
+        tokens = [np.array([1, 2, 3]), np.array([4, 5, 6]), np.array([7, 8, 9])]
+        qids = [1, 2, 3]
+        return tokens, qids
+
+    @pytest.mark.parametrize("compress", [False, True])
+    def test_npz_saver_uncompressed(self, tmp_path, saver_data, compress):
+        filename = str(tmp_path / f"test_{compress}.npz")
+        tokens, qids = saver_data
+        input_gen = zip(tokens, qids)
+
+        saver = NPZSaverIncremental(filename, save_every=2, compress=compress)
+        list(saver.process(input_gen))
+
+        assert os.path.exists(tmp_path / f"test_{compress}_1.npz")
+        assert os.path.exists(tmp_path / f"test_{compress}_2.npz")
+        loaded_data = np.load(tmp_path / f"test_{compress}_1.npz")
+        assert "tokens" in loaded_data
+        assert "qids" in loaded_data
+        np.testing.assert_array_equal(loaded_data["tokens"], np.array(tokens[:2]))
+        np.testing.assert_array_equal(loaded_data["qids"], np.array(qids[:2]))
