e5e0c10f01ec06cbf977d9f3371e61c258c09b6d
Dominik Farhan <nikousf@seznam.cz>
2025-08-03 22:24:28 +0200

    feat(train): :sparkles: add option to run all recalls from one python function

--- src/finetunings/evaluation/evaluate.py
+++ src/finetunings/evaluation/evaluate.py
@@ -1,6 +1,19 @@
+import logging
+
 from finetunings.evaluation.find_recall import find_recall
 
 _RECALLS = [1, 10, 100]
+
+_logger = logging.getLogger("finetuning.evaluation.evaluate")
+
+
+def _construct_mewsli_path(root_dir: str, finetuning_round: int, lang: str) -> str:
+    return f"{root_dir}/mewsli_embs_{lang}_{finetuning_round}"
+
+
+def _construct_damuel_path(root_dir: str, finetuning_round: int) -> str:
+    next_finetuning_round = finetuning_round + 1
+    return f"{root_dir}/damuel_for_index_{next_finetuning_round}"
 
 
 def run_recall_calculation(damuel_dir, mewsli_dir, recall=None):
@@ -9,15 +22,13 @@ def run_recall_calculation(damuel_dir, mewsli_dir, recall=None):
 
 
 def evaluate(
-    damuel_desc_tokens,
-    mewsli_tokens,
-    model_path,
-    damuel_dir,
-    mewsli_dir,
-    state_dict=None,
+    root_dir: str,
+    finetuning_round: int,
+    langs: list[str] = ["ar", "de", "en", "es", "ja", "fa", "sr", "ta", "tr"],
 ):
-    raise NotImplementedError()
+    damuel_path = _construct_damuel_path(root_dir, finetuning_round)
 
-
-if __name__ == "__main__":
-    evaluate()
+    for lang in langs:
+        mewsli_path = _construct_mewsli_path(root_dir, finetuning_round, lang)
+        _logger.info(f"Calculating recall for {lang}")
+        run_recall_calculation(damuel_path, mewsli_path)
